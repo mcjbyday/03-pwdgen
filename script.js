03e50f268163979458a6bec2e52cceb8064540bc
MCJBYDAY <jacobsmichaelc@gmail.com>
2022-03-30 16:55:25 -0500

    added test strings to use to build the password generator logic

--- script.js
+++ script.js
@@ -5,8 +5,13 @@ var generateBtn = document.querySelector("#generate");
 // 1) get preferred length
 // 2) get input to build possibility space of availability characters
 // 3) randomly select characters from the built array and assemble the password
-let pwdL = prompt("How long do you want this password to be?");
+let pwdL = prompt("How many characters would you like your password to have?");
 
+let letterLowerString = "abcdefghijklmnopqrstuvwxyz";
+let letterUpperString = "ABCDEFGHIJKLMNOPQRSTYUVWXYZ";
+let numericString = "0123456789";
+//special operators used for string literals
+let specialString = " !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
 
 
 // TODO error handling to make sure input is under 129
